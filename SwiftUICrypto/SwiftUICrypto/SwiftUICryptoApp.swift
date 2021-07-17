1864930d14104770bc91cf16c688f78f8f1c051c
Rahul K <rahul.koduri@me.com>
2021-07-17 19:17:15 +1000

    setup
    XCode project

---
+++ SwiftUICrypto/SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Rahul on 17/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
