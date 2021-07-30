18741e100cf85be5c95e2559a1b59e7bc6f800b2
Rahul K <rahul.koduri@me.com>
2021-07-30 15:00:38 +1000

    Data Service
    added a data service class that fetches json data using combine

---
+++ SwiftUICrypto/SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewModel.swift
+//  SwiftUICrypto
+//
+//  Created by Rahul on 30/7/21.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
