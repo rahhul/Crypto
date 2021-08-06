ab137c59032fa4c12a523eef2e8b5df29e7dbdd9
Rahul K <rahul.koduri@me.com>
2021-08-06 14:48:42 +1000

    changes in sorting and lazy navigationlink

--- SwiftUICrypto/SwiftUICrypto/Core/Detail/Views/DetailView.swift
+++ SwiftUICrypto/SwiftUICrypto/Core/Detail/Views/DetailView.swift
@@ -7,22 +7,36 @@
 
 import SwiftUI
 
-struct DetailView: View {
+struct DetailLoadingView: View {
     
     @Binding var coin: CoinModel?
     
-    init(coin: Binding<CoinModel?>) {
-        self._coin = coin
-        print("Initializing Detail View for \(coin.wrappedValue?.name)")
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+    
+}
+
+struct DetailView: View {
+    
+    @StateObject var vm: DetailViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+        print("Initializing Detail View for \(coin.name)")
     }
     
     var body: some View {
-        Text(coin?.name ?? "")
+        Text("hello")
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: .constant(dev.coin))
+        DetailView(coin: dev.coin)
     }
 }
